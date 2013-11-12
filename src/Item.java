a33b9c8167c7f87ef18ec4f4e31b7b95323c8860
Austin Turner <paustint@gmail.com>
2013-11-11 22:34:30 -0700

    Added functionality to remove item from order and add additional items to order

--- src/Item.java
+++ src/Item.java
@@ -8,8 +8,8 @@ public class Item {
 	private int itemID;
 	private double itemPrice;
 	private String itemComment = "";
-	NumberFormat formatMoney = NumberFormat.getCurrencyInstance();
-	
+	private NumberFormat formatMoney = NumberFormat.getCurrencyInstance();
+	private int orderMenuItemID; //This is used to get the actual item on the actual order (as opposed to just the item) for changes, i.e. deletion
 	
 //	public Item(String name,int num, double price){
 //		setItemName(name);
@@ -26,6 +26,27 @@ public class Item {
 		setCategory(category);
 		
 	}
+	/**
+	 * Overloaded constructor for DB to create items
+	 * 
+	 * @param name
+	 * @param description
+	 * @param category
+	 * @param num
+	 * @param price
+	 * @param comment
+	 * @param orderMenuItemID
+	 */
+	public Item(String name,String description, String category, int num, double price, String comment, int orderMenuItemID){
+		setItemName(name);
+		setitemID(num);
+		setItemPrice(price);
+		setDescription(description);
+		setCategory(category);
+		this.orderMenuItemID = orderMenuItemID;
+		itemComment = comment;
+	}
+	
 	public Item(String name,String description, String category, int num, double price, String comment){
 		setItemName(name);
 		setitemID(num);
@@ -35,11 +56,20 @@ public class Item {
 		setItemComment(comment);
 		
 	}
+	
 	public Item(){}
 
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
+	
+	/**
+	 * Return orderMenuitemID
+	 * @return
+	 */
+	public int getOrderMenuItemID(){
+		return orderMenuItemID;
+	}
 	 
 	public String toStringFull() {
 		return  itemName + ", item number: " + itemID
